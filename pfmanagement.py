5c6ba4e4425898e88f42afb3d7c80d25b200896b
manuellopper <96139661+manuellopper@users.noreply.github.com>
2021-12-18 19:31:55 +0100

    Create pfmanagement.py

---
+++ pfmanagement.py
@@ -0,0 +1,24 @@
+class Portfolio:
+  def __init__(self):
+    
+class Asset:
+  def __init__(self):
+    
+class AssetEquity(Asset):
+  def __init__(self):
+    
+class Transaction:
+  def __init__(self):
+    
+class TransactionBuyEquity(Transaction):
+  def __init__(self):
+    
+class TransactionSellEquity(Transaction):
+  def __init__(self):
+    
+class TransactionDividendEquity(Transaction):
+  def __init__(self):
+      
+class TransactionDividendWithSharesEquity(Transaction):
+  def __init__(self):
+  
