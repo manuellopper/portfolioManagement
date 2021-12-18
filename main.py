5fba620aed005bfc3487ef2f36936f040228a20e
manuellopper <96139661+manuellopper@users.noreply.github.com>
2021-12-18 20:51:51 +0000

    definiciones

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from pfmanager.engine import *
 
 
-a=Portfolio("Portfolio de Prueba")
-b=Asset()
-c=AssetEquity()
+pf=Portfolio("Portfolio de Prueba")
+as1=Asset("Telefónica SA","EUR",pf)
+as2=AssetEquity("Iberdrola","EUR","IBR",pf)
 d=Transaction()
 e=TransactionBuyEquity()
 f=TransactionSellEquity()
 g=TransactionDividendEquity()
 h=TransactionDividendWithSharesEquity()
 
-print(a.pf_name)
+print(pf.pf_name)
+print(f"Asset - Id: {as1.get_id()} Asset Type: {as1.asset_type} Nombre: {as1.asset_name} Currency: {as1.currency} Portfolio: {as1.portfolio.pf_name}")
+print(f"Asset - Id: {as2.get_id()} Asset Type: {as2.asset_type} Nombre: {as2.asset_name} Symbol: {as2.symbol} Currency: {as2.currency} Portfolio: {as2.portfolio.pf_name}")
 
+
+print(d.get_id())
+print(e.get_id())
+print(f.get_id())
+print(g.get_id())
+print(h.get_id())
 
--- pfmanager/engine.py
+++ pfmanager/engine.py
@@ -1,32 +1,81 @@
 class Portfolio:
   def __init__(self,name):
     self.pf_name=name
+    self.assets_list=[]
+    self.transactions_list=[]
+
 
 class Asset:
-  def __init__(self):
-    self.dummie=0
+  seed_id = [0]
+  asset_type="Undertermined"
+  
+  def __init__(self,name,currency,pf_father):
+    self.set_new_id()
+    self.asset_name=name
+    self.currency=currency
+    self.portfolio = pf_father
+    self.transactions_list=[]
+    
+  def set_new_id(self):     
+    self.id=int(self.seed_id[0])+1
+    self.seed_id[0]=self.id
+  
+  def get_id(self):
+    return self.id
+
+
     
 class AssetEquity(Asset):
-  def __init__(self):
-    self.dummie=0
+
+  def __init__(self, name, currency, symbol, pf_father, sector=None,market_type=None, size=None, caract=None):
+    # Main information
+    super().__init__(name,currency,pf_father)    
+    self.asset_type="Equity"
+    self.symbol = symbol
+    #Asset general information
+    self.sector=sector
+    self.market_type=market_type
+    self.size=size
+    self.caract=caract
+    #Asset internal KPI
+    self.curr_shares = 0
+    self.market_value = 0
+    self.total_buy_shares =0
+    self.total_sell_shares =0 
+    
+
+
+
+
+
+
+
     
 class Transaction:
+  seed_id = [0]
   def __init__(self):
-    self.dummie=0
+    self.set_new_id()
+
+  def set_new_id(self):     
+    self.id=int(self.seed_id[0])+1
+    self.seed_id[0]=self.id
+
+  def get_id(self):
+    return self.id
     
 class TransactionBuyEquity(Transaction):
   def __init__(self):
-    self.dummie=0
+    self.set_new_id()
     
 class TransactionSellEquity(Transaction):
   def __init__(self):
-    self.dummie=0
+    self.set_new_id()
     
 class TransactionDividendEquity(Transaction):
   def __init__(self):
-    self.dummie=0
+    self.set_new_id()
       
 class TransactionDividendWithSharesEquity(Transaction):
   def __init__(self):
-    self.dummie=0
+    self.set_new_id()
   
